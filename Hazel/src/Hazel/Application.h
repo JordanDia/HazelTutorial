d381f3da3ef6c5ed7fc9d30192603fcaf0cbaa27
Jordan Dia <jordandia101@gmail.com>
2022-03-18 23:23:38 -0400

    Finished ImGui Events.

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,6 +1,12 @@
 #pragma once
 
 #include "Core.h"
+	
+#include "Window.h"
+
+#include "LayerStack.h"
+#include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
 
 namespace Hazel {
 
@@ -11,6 +17,22 @@ namespace Hazel {
 		virtual ~Application();
 
 		void Run();
+		
+		void OnEvent(Event& e);
+
+		void PushLayer(Layer* layer);
+		void PushOverlay(Layer* layer);
+
+		inline Window& GetWindow() { return *m_Window; }
+
+		inline static Application& Get() { return *s_Instance; }
+	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
+		LayerStack m_LayerStack;
+	private:
+		static Application* s_Instance;
 	};
 
 	// To be defined in CLIENT
