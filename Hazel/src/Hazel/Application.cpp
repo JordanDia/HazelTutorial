88dff4370f80201508fe1c58f1e305947bbe0f5d
Jordan Dia <jordandia101@gmail.com>
2022-06-04 19:29:29 -0400

    Moved code from Application to Sandbox. Created movement and rotation for the camera.

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -14,7 +14,6 @@ namespace Hazel {
 	Application* Application::s_Instance = nullptr;
 	
 	Application::Application()
-		: m_Camera(-1.6f, 1.6f, -0.9f, 0.9f)
 	{
 		HZ_CORE_ASSERT(!s_Instance, "Application already exists!");
 		s_Instance = this;
@@ -24,123 +23,10 @@ namespace Hazel {
 
 		m_ImGuiLayer = new ImGuiLayer();
 		PushOverlay(m_ImGuiLayer);
-
-		// Rendering a triangle
-
-		m_VertexArray = std::shared_ptr<VertexArray>(VertexArray::Create());
-		//m_VertexArray.reset(VertexArray::Create());
-		float vertices[3 * 7] = {
-			-0.5f, -0.5f, +0.0f, +0.6f, +0.8f, +1.0f, +1.0f,
-			+0.5f, -0.5f, +0.0f, +0.0f, +0.0f, +1.0f, +1.0f,
-			+0.0f, +0.5f, +0.0f, +0.2f, +0.9f, +1.0f, +1.0f
-		};
-
-		std::shared_ptr<VertexBuffer> vertexBuffer = std::shared_ptr<VertexBuffer>(VertexBuffer::Create(vertices, sizeof(vertices)));
-
-		BufferLayout layout = {
-			{ ShaderDataType::Float3, "a_Position" },
-			{ ShaderDataType::Float4, "a_Color" },
-		};
-		vertexBuffer->SetLayout(layout);
-		m_VertexArray->AddVertexBuffer(vertexBuffer);
-
-		unsigned int indices[3] = { 0, 1, 2 };
-		std::shared_ptr<IndexBuffer> indexBuffer = std::shared_ptr<IndexBuffer>(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
-		m_VertexArray->SetIndexBuffer(indexBuffer);
-
-		m_SquareVA = std::shared_ptr<VertexArray>(VertexArray::Create());
-		//m_SquareVA.reset(VertexArray::Create());
-		float squareVertices[4 * 3] = {
-			-0.75f, -0.75f, +0.0f,
-			+0.75f, -0.75f, +0.0f,
-			+0.75f, +0.75f, +0.0f,
-			-0.75f, +0.75f, +0.0f
-		};
-
-		std::shared_ptr<VertexBuffer> squareVB = std::shared_ptr<VertexBuffer>(VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
-
-		squareVB->SetLayout({
-			{ ShaderDataType::Float3, "a_Position" }
-			});
-		m_SquareVA->AddVertexBuffer(squareVB);
-
-		unsigned int squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
-		std::shared_ptr<IndexBuffer> squareIB = std::shared_ptr<IndexBuffer>(IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
-		m_SquareVA->SetIndexBuffer(squareIB);
-
-		std::string vertexSource = R"(
-			#version 330 core
-			
-			layout(location = 0) in vec3 a_Position;
-			layout(location = 1) in vec4 a_Color;
-
-			uniform mat4 u_ViewProjection;
-			
-			out vec3 v_Position;
-			out vec4 v_Color;
-
-			void main()
-			{
-				v_Position = a_Position;
-				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
-			}
-		)";
-
-		std::string fragmentSource = R"(
-			#version 330 core
-			
-			layout(location = 0) out vec4 color;
-
-			in vec3 v_Position;
-			in vec4 v_Color;
-
-			void main()
-			{
-				color = vec4(v_Position * 0.5 + 0.5, 1.0);
-				color = v_Color;
-			}
-		)";
-
-		m_Shader = std::make_shared<Shader>(vertexSource, fragmentSource);
-		//m_Shader.reset(new Shader());
-
-		std::string blueShaderVertexSource = R"(
-			#version 330 core
-			
-			layout(location = 0) in vec3 a_Position;
-
-			uniform mat4 u_ViewProjection;
-			
-			out vec3 v_Position;
-
-			void main()
-			{
-				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
-			}
-		)";
-
-		std::string blueShaderFragmentSource = R"(
-			#version 330 core
-			
-			layout(location = 0) out vec4 color;
-
-			in vec3 v_Position;
-
-			void main()
-			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
-			}
-		)";
-
-		m_BlueShader = std::make_shared<Shader>(blueShaderVertexSource, blueShaderFragmentSource);
-		//m_Shader.reset(new Shader());
 	}
 
 	Application::~Application()
 	{
-		
 	}
 
 	void Application::PushLayer(Layer* layer)
@@ -166,27 +52,12 @@ namespace Hazel {
 			if (e.Handled)
 				break;
 		}
-
-		//HZ_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
 	{
 		while (m_Running)
 		{
-			RenderCommand::SetClearColor({ 0.15f, 0.15f, 0.15f, 1.0f });
-			RenderCommand::Clear();
-
-			m_Camera.SetPosition({ 0.5f, 0.5f, 0.0f });
-			m_Camera.SetRotation(45.0f);
-
-			Renderer::BeginScene(m_Camera);
-
-			Renderer::Submit(m_SquareVA, m_BlueShader);
-			Renderer::Submit(m_VertexArray, m_Shader);
-			
-			Renderer::EndScene();
-			
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
  
