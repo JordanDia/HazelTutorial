9c579bac7d4b63ee0eaede75a1d9dd29ec2d05a9
Jordan Dia <jordandia101@gmail.com>
2022-07-14 19:36:14 -0400

    Moved Hazel files into Core folder.

---
+++ Hazel/src/Hazel/Core/Input.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Hazel/Core/Core.h"
+
+namespace Hazel {
+
+	class HAZEL_API Input
+	{
+	public:
+		static bool IsKeyPressed(int keycode);
+
+		static bool IsMouseButtonPressed(int button);
+		static std::pair<float, float> GetMousePosition();
+		static float GetMouseX();
+		static float GetMouseY();
+	};
+
+}
