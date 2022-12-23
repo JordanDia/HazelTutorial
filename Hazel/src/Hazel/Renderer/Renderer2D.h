75ab4b9fbff6ade074fd8f08afcf1d458997c281
Jordan Dia <jordandia101@gmail.com>
2022-12-23 17:30:20 -0500

    Added Batch Rendering for textures. Video 62.

--- Hazel/src/Hazel/Renderer/Renderer2D.h
+++ Hazel/src/Hazel/Renderer/Renderer2D.h
@@ -26,8 +26,5 @@ namespace Hazel {
 		static void DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const glm::vec4& color);
 		static void DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor = 0.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
 		static void DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor = 0.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
-
-		
 	};
-
 }
--- Hazel/src/Hazel/Renderer/Texture.h
+++ Hazel/src/Hazel/Renderer/Texture.h
@@ -17,6 +17,8 @@ namespace Hazel {
 		virtual void SetData(void* data, uint32_t size) = 0;
 		
 		virtual void Bind(uint32_t slot = 0) const = 0;
+
+		virtual bool operator==(const Texture& other) const = 0;
 	};
 
 	class Texture2D : public Texture
@@ -25,5 +27,4 @@ namespace Hazel {
 		static Ref<Texture2D> Create(uint32_t width, uint32_t height);
 		static Ref<Texture2D> Create(const std::string& path);
 	};
-
 }
